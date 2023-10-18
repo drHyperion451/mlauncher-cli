8bb2ba4d90305a319bebdaa0cfe9399943cb59a5
drHyperion451 <doctorhyperion451@gmail.com>
2023-10-18 17:26:09 +0100

    Use detected home directory instead

--- src/globals.py
+++ src/globals.py
@@ -1,8 +1,13 @@
+from pathlib import Path
+
 # GLOBAL PARAMS
+# Some of them are placeholders. TODO: Make configs and change the params
+home = Path.home()
+
 JSON_FILEPATH = 'src/ml_info.json'
-SOURCEPORT = '/Users/drHyperion451/Documents/PROYECTOS/dsda-doom/build/dsda-doom'
-IWAD = '/Users/drHyperion451/Documents/PROYECTOS/dsda-doom/build/DOOM2.WAD'
-ML_PATH = '/Users/drHyperion451/Documents/PROYECTOS/dsda-doom/build/wads/master_levels'
+SOURCEPORT = f'{home}/Documents/PROYECTOS/dsda-doom/build/dsda-doom'
+IWAD = f'{home}/Documents/PROYECTOS/dsda-doom/build/DOOM2.WAD'
+ML_PATH = f'{home}/Documents/PROYECTOS/dsda-doom/build/wads/master_levels'
 SELECTED_FILE = 'ATTACK.WAD'
 
 # GLOBAL BOOLS
