f3aa877b417c503ace2472d93f122d5ca75038d2
drHyperion451 <doctorhyperion451@gmail.com>
2023-10-28 17:24:45 +0100

    Support for config files.

---
+++ build.bat
@@ -0,0 +1,1 @@
+pyinstaller src/main.py -y -D --add-data src/ml_info.json:src --add-data src/css/mlauncher.tcss:css -n MLauncher --paths=E:\Users\sbritorodr\Documentos\GitHub\mlauncher-cli\pyenv\Lib\site-packages
