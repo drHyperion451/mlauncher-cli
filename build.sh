8d265e0717c270e61e674c935d36fcfac6f5da32
drHyperion451 <doctorhyperion451@gmail.com>
2023-10-28 16:32:13 +0100

    Advanced build script

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
-pyinstaller src/main.py -y -F --add-data src/ml_info.json:src --add-data src/css/mlauncher.tcss:css -n 'MLauncher' --paths='/usr/local/lib/python3.11/site-packages'
+site_packages=$(python3 -c 'import sysconfig; print(sysconfig.get_paths()["purelib"])')
+
+PYTHONOPTIMIZE=2 pyinstaller src/main.py -y -D --add-data src/ml_info.json:src --add-data src/css/mlauncher.tcss:css -n 'MLauncher' --paths=$site_packages
 
 #./dist/MLauncher/MLauncher
