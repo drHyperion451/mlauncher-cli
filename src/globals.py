630f6791e9bd7bf655660aa84a552cb5c969703b
drHyperion451 <doctorhyperion451@gmail.com>
2023-10-28 13:38:32 +0100

    Compilation for MacOS

--- src/globals.py
+++ src/globals.py
@@ -1,8 +1,15 @@
 from pathlib import Path
+import sys
+import os
 
 # GLOBAL PARAMS
 # Some of them are placeholders. TODO: Make configs and change the params
 home = Path.home()
+
+try: 
+    os.chdir(sys._MEIPASS)
+except (NameError, AttributeError) as error:
+    print(f"{error}. Expected to be running as script, not frozen.")
 
 JSON_FILEPATH = 'src/ml_info.json'
 SELECTED_FILE = 'ATTACK.WAD'
