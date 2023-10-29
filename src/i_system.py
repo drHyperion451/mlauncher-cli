07894f11e442f1240923e266545b72d1300ca16d
drHyperion451 <doctorhyperion451@gmail.com>
2023-10-29 18:24:45 +0000

    Added openFile handler depending on OS

---
+++ src/i_system.py
@@ -0,0 +1,12 @@
+import platform
+import os
+import sys
+import webbrowser
+OS = platform.system()
+
+def openFile(filePath):
+    match OS:
+        case 'Windows', 'Linux': # Check: Linux works?
+            webbrowser.open(filePath)
+        case 'Darwin':
+            os.system(f'open {filePath}')
