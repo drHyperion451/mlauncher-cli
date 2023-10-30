e3429c83e54dc0aea98e645a1a56006b7ef24c80
drHyperion451 <doctorhyperion451@gmail.com>
2023-10-30 22:09:06 +0000

    Cleanup

--- src/ui_settings.py
+++ src/ui_settings.py
@@ -6,7 +6,6 @@ from textual.widgets import Label, Button, Placeholder, Static, OptionList, \
 Checkbox, Input
 
 from globals import *
-from i_jsonUtils import MapsJson
 
 
 class SettingsScreen(Screen):
@@ -25,48 +24,3 @@ class SettingsScreen(Screen):
         match event.button.id:
             case 'optionsCancel':
                 self.app.pop_screen()
-
-class pwadLaunchOptions(Static):
-    def compose(self) -> ComposeResult:
-        yield Grid(
-            Grid(
-                Checkbox("[b]Skill Level:[/b]", id='skill-level-button', 
-                         classes='launchOptionButton skillOptionElement'),
-                OptionList(# id: skillList classes:skillOptionElement
-                    "I'm too young to die",
-                    "Hey, not too rough",
-                    "Hurt me plenty",
-                    "Ultra-Violence",
-                    "Nightmare!",
-                    id='skillList',
-                    classes="skillOptionElement",
-                    disabled=False,
-                    wrap=False,
-                ),
-                id="skillLevelGrid",
-                classes="innerLaunchOptsGrid"
-            ),
-            Grid(
-                Horizontal(
-                    Vertical(
-                        Input(id="warp-input", placeholder="Warp Map", 
-                              classes="misc-option-element"),
-                        Checkbox("Fast   ", classes="misc-option-element"),
-                        Checkbox("Respawn", classes="misc-option-element"),
-                        id="nightmare-opts-grid",
-                    ),
-                    Vertical(
-                        # This is really bad, but I'm tired of fixing things
-                        Checkbox("Point to ML", id='auto-warp-checkbox', 
-                                 classes="misc-option-element"),
-                        Checkbox("No Cheats  ", classes="misc-option-element"), # Must have 11 spaces
-                        Checkbox("No Monsters", classes="misc-option-element"),
-                        id="weird-opts-grid",
-                    ),
-                    id="flags-option-grid",
-                ),
-                id="miscGrid",
-                classes="innerLaunchOptsGrid"
-            ),
-            id="pwadLaunchOptionsGrid"
-        )
