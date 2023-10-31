e36d3887f898d0ca4751f51cb7f7f3cef3b1cc1f
drHyperion451 <doctorhyperion451@gmail.com>
2023-10-31 23:00:21 +0000

    Fixed globals error

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ import g_launch
 from ui_quit import QuitScreen
 from ui_settings import SettingsScreen
 from ui_pwad_launch_options import pwadLaunchOptions
-from globals import *
+from globals import GlobalVars
 
 # Note: CSS id SHOULD be the same name as the class... wasted 2h of my life
 
@@ -47,20 +47,21 @@ class pwadMain(Static):
             pwadList(id='pwadList'),
             pwadContents(id='pwadContents')
         )
+    # WADS LIST HANDLER:
     def on_button_pressed(self, event: Button.Pressed) -> None:
         # Refresh if the user selects map:
         if 'map-buttons' in event.button.classes:
-            SELECTED_MAP = event.button.id + '.WAD'
+            GlobalVars.SELECTED_MAP = event.button.id + '.WAD'
             # Change the title
-            self.query_one("#title").str_body = SELECTED_MAP
-            self.query_one('#author').str_body = maps.get_from_data('WAD', SELECTED_MAP, 'Author')[0]
+            self.query_one("#title").str_body = GlobalVars.SELECTED_MAP
+            self.query_one('#author').str_body = maps.get_from_data('WAD', GlobalVars.SELECTED_MAP, 'Author')[0]
             # Change WARP_PC:
-            WARP_PC = maps.get_from_data('WAD', SELECTED_MAP, 'PC')[0]
+            GlobalVars.WARP_PC = maps.get_from_data('WAD', GlobalVars.SELECTED_MAP, 'PC')[0]
             
 class pwadList(VerticalScroll):
 
     def compose(self) -> ComposeResult:
-        wads = maps.get_all_wads_ordered(WADS_ORDER, displayExtension=False)
+        wads = maps.get_all_wads_ordered(GlobalVars.WADS_ORDER, displayExtension=False)
 
         for eachWad in wads:
             yield Button(eachWad, id=eachWad, classes="map-buttons")
@@ -80,14 +81,15 @@ class pwadContents(Static):
         yield pwadInfo()
         yield pwadLaunchOptions(id='pwadLaunchOptions')
         yield Button("LAUNCH", variant='success', id='launchButton')
-
+    
+    #  LAUNCH HANDLER:
     def on_button_pressed(self, event: Button.Pressed) -> None:
         if event.button.id == 'launchButton':
-            if SELECTED_MAP:
-                master_wad = f"{ML_PATH}/{SELECTED_MAP}"
-                g_launch.game(SOURCEPORT, IWAD, master_wad)
-                config.set('GAME', 'SELECTED_MAP', SELECTED_MAP)
-                config.set('GAME', 'SKILL', SKILL)
+            if GlobalVars.SELECTED_MAP:
+                master_wad = f"{GlobalVars.ML_PATH}/{GlobalVars.SELECTED_MAP}"
+                g_launch.game(GlobalVars.SOURCEPORT, GlobalVars.IWAD, master_wad)
+                config.set('GAME', 'SELECTED_MAP', GlobalVars.SELECTED_MAP)
+                config.set('GAME', 'SKILL', str(GlobalVars.SKILL))
                 #config.set('GAME', 'WARP', WARP)
                 with open(SETTINGS_PATH, 'w') as configfile:
                     config.write(configfile)
@@ -113,7 +115,7 @@ class MLauncherApp(App):
     def action_request_quit(self) -> None:
         """ Action that quits the app."""
 
-        if QUICK_EXIT: self.app.exit()
+        if GlobalVars.QUICK_EXIT: self.app.exit()
         self.push_screen(QuitScreen(classes='DialogScreen'))
 
     def action_request_settings(self) -> None:
