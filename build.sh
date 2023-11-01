27a9b593fa2780d8da60da4be59130cc5fc52985
drHyperion451 <doctorhyperion451@gmail.com>
2023-11-01 13:01:29 +0000

    Updated build scripts
    Now it copies README.md and COPYING files

--- build.sh
+++ build.sh
@@ -2,4 +2,7 @@ site_packages=$(python3 -c 'import sysconfig; print(sysconfig.get_paths()["purel
 
 PYTHONOPTIMIZE=2 pyinstaller src/main.py -y -D --add-data src/ml_info.json:src --add-data src/css/mlauncher.tcss:css -n 'MLauncher' --paths=$site_packages
 
+# Copies and pastes required files for bundling
+cp README.MD dist/MLauncher/README.MD
+cp COPYING dist/MLauncher/COPYING
 #./dist/MLauncher/MLauncher
