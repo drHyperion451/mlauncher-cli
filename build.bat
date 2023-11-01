0533dba0edf9507409ec6cc7a739bb921ab8b1da
drHyperion451 <doctorhyperion451@gmail.com>
2023-11-01 13:01:29 +0000

    Updated build scripts
    Now it copies README.md and COPYING files

--- build.bat
+++ build.bat
@@ -2,3 +2,6 @@ Rem Maybe you need to run this on PS:  Set-ExecutionPolicy Unrestricted -Scope P
 Rem #pyright: reportUndefinedVariable=false at the top of any python file will ignore imports
  
 pyinstaller src/main.py -y -D --add-data src/ml_info.json:src --add-data src/css/mlauncher.tcss:css -n MLauncher --paths=E:\Users\drhyperion\Documentos\GitHub\mlauncher-cli\pyenv\Lib\site-packages
+
+copy README.MD dist\MLauncher\README.MD
+copy COPYING dist\MLauncher\COPYING
